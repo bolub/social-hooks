9503f68bfb5b0b1611c44abb41f8766e479ac9a4
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2024-02-13 11:51:21 +0200

    Trying from scratch config

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { useSocialAssets } from './hooks/useSocialAssets';
-export { useSocialShare } from './hooks/useSocialShare';
-export type { Social, SocialAsset, SocialAssetColor } from './types';
+export const hello = () => {
+  return "hi";
+};
